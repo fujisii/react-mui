c2d39ae3896a0bd6b1acf9a3a2a3d9d84707fc1a
fujisii <51264835+fujisii@users.noreply.github.com>
2022-07-06 12:49:30 +0900

    Add: card（実装中）

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./styles.css";
 import MenuBar from "./components/atoms/menuBar";
+import RecipeReviewCard from "./components/atoms/recipeReviewCard";
 
 export default function App() {
   return (
     <div className="App">
       <MenuBar />
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <RecipeReviewCard />
+      <RecipeReviewCard />
     </div>
   );
 }
