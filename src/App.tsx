e76271b550d0936b50fef7a4428589e1cc72a141
fujisii <51264835+fujisii@users.noreply.github.com>
2022-07-06 12:14:18 +0900

    - Add: package @mui/icons-material, Add: AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import "./styles.css";
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 export default function App() {
   return (
     <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" component="div">
+              Photos
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
