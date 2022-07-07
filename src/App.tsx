d0f36b3083af4591c899dbfa7b78937fc5297c71
fujisii <51264835+fujisii@users.noreply.github.com>
2022-07-07 12:28:17 +0900

    add: BasicModal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./styles.css";
 import MenuBar from "./components/atoms/menuBar";
 import RecipeReviewCard from "./components/atoms/recipeReviewCard";
+import BasicModal from "./components/atoms/basicModal";
 
 export default function App() {
   return (
     <div className="App">
       <MenuBar />
+      <BasicModal />
       <RecipeReviewCard />
       <RecipeReviewCard />
     </div>
